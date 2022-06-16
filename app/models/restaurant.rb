a349b709895d5861eb3764793fd6ac561799374d
stolarz <stolarz92@gmail.com>
2022-06-16 20:07:52 +0200

    Publish data to the queue when restaurant is created

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,9 @@
 class Restaurant < ApplicationRecord
+  after_create { publish_to_dashboard }
+  
+  private
+
+  def publish_to_dashboard
+    Publisher.publish('restaurants', attributes)
+  end
 end
